cfd461d9fcddf4d28c129743fca56e365e5a283a
Alex Makarov <rubenen@mail.ru>
2022-07-15 19:16:57 +0300

    commit

--- index.js
+++ index.js
@@ -8,23 +8,27 @@ let scrape = async () => {
     const browser = await puppeteer.launch(); //Открывает браузер
     const page = await browser.newPage(); // Создает страницу
     await page.goto('https://ria.ru/lenta/'); // Заходит на нужный адрес
-
-    await page.waitForSelector('.list-item__title'); //Жду пока загрузиться селектор
+    await page.click('.list-more');
+    await page.waitForSelector('.list-item__title');
+    await page.waitForSelector('.list-item__image');
+    await page.waitForSelector('.list-more'); //Жду пока загрузиться селектор
     await page.setViewport({
         width: 2000,
-        height: 3000
+        height: 30000
     }); // Задаем ширину страницы 
     const result = await page.evaluate(() => {
         let data = [];
-        let elements = document.querySelectorAll('.list-item__title');
-        for (let element of elements) {
-            data.push({title: element.innerText});
+        let elements = document.querySelectorAll('.list-item');
+        // let titles = document.querySelectorAll('.list-item__title');
+        // let images = document.querySelectorAll('.list-item__image');
+        for (let elemtnt of elements) {
+            data.push({ title: elemtnt.querySelector('.list-item__title').innerText, image: elemtnt.querySelector('img').getAttribute('src') });
         }
         return data;
     });
     console.log(result);
     fs.writeFile('result.json', JSON.stringify(result, null, ' '), err => {
-        if(err) return err;
+        if (err) return err;
         console.log(`картинкаи пришли`);
     });
     await browser.close();
@@ -32,7 +36,7 @@ let scrape = async () => {
 
 setInterval(() => {
     scrape();
-}, 10000);
+}, 100000);
 
 // https://ria.ru/lenta/
 // .list - item__content
--- main.js
+++ main.js
@@ -7,13 +7,16 @@ request.send();
 const newsPage = document.querySelector('.news');
 console.log(newsPage);
 
-request.addEventListener('readystatechange', ()=> {
+request.addEventListener('readystatechange', () => {
     if (request.readyState === 4 && request.status === 200) {
         let news = JSON.parse(request.response);
         Array.from(news);
-        console.log(news)    
+        console.log(news)
         for (let itemNews = 0; itemNews < news.length; itemNews++) {
-            newsPage.innerHTML += `<div class="news__item">${news[itemNews].title}</div>`;
+            newsPage.innerHTML += `<div class="news__item item-news">
+                <h2 class="item-news__title">${news[itemNews].title}</h2>
+                <img  class="item-news__img" src="${news[itemNews].image}" alt="">
+            </div>`;
         }
     }
 });
